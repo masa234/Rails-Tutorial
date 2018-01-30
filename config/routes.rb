b7e79ecb6dd6ab9e6e6ee4d7020e390689567282
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-01-30 02:56:33 +0000

    remake toppage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#root'
-  get  '/signup',  to: 'users#new', as: "signup"
+  get  'user/signup',  to: 'users#new', as: "signup"
   resources :users
 end
