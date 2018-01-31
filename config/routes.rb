cc727d33489ef7b644a8e11dd03593ef285d1388
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-01-31 04:15:11 +0000

    create sessions controller && routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#root'
   get  'user/signup',  to: 'users#new', as: "signup"
+  
+  get  'user/login',  to: 'users#new', as: "login"
+  post  'user/login',  to: 'users#new'
+  delete  'user/logout',  to: 'users#logout', as: "logout"
   resources :users
 end
