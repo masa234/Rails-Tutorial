4e4f9a51531df8afec112cc659bafe3013fb4617
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-01-31 05:26:04 +0000

    ログイン、ログアウトによってトップページのレイアウトを切り替えるように設定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      redirect_to "/"
     else
       swal { error 'ログインに失敗しました' }
       render:new
