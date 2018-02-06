614bb17d2d23bf1d3003f90098775d2051314922
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-02-06 05:50:45 +0000

    フォロー機能を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
   
   has_many :microposts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   
   def User.digest(string)
@@ -58,7 +58,7 @@ class User < ApplicationRecord
     relationship.destroy if relationship
   end
   
-  def follow?(other_user)
-    self.following.include?(other_user)
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
