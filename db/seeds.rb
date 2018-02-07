fcda2dd20e915e67c7c152d06ec35ed547ad7f51
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-02-07 19:40:12 +0000

    heroku run rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(name:  "Example User",
-#             email: "bfmt1250081@gn.iwasaki.ac.jp",
-#             nickname: "example",
-#             password:              "password",
-#             password_confirmation: "password",
-#             admin: true)
 
 99.times do |n|
   name  = Faker::Lorem.sentence
