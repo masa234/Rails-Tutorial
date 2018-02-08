19da31706ebd021e9b0a05226402fbad28e684d7
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-02-08 16:47:15 +0000

    toppage

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def root
     if sign_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed(current_user)
+      @feed_items = current_user.feed(current_user).page(params[:page])
     end
   end
 end
