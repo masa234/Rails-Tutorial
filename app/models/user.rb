d0bc85ed8cf35e46b6f32b544311d91e4c4c14d4
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-02-11 07:57:33 +0000

    set user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email.downcase! }
   before_save { self.nickname.gsub!(" ", "") }
-  validates :name,  presence: true#, length: { maximum: 20 }
-  validates :nickname, presence: true,  uniqueness: true#, length: {maximum: 15}, allow_nil: true
+  validates :name,  presence: true, length: { maximum: 20 }
+  validates :nickname, presence: true,  uniqueness: true, length: {maximum: 15}, allow_nil: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
